70db3b643d4e5d98acf48cec983902143670ea5a
TARGETrus <mousebreaker@mail.ru>
2014-06-28 15:04:50 +0400

    commit #2

---
+++ Ivanov-Ivan/src/com/javaschool2014/task1/Main.java
@@ -0,0 +1,16 @@
+package com.javaschool2014.task1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Available command list:");
+        System.out.print("Create <name><timezone><active>, Create <name><timezone><active>, AddEvent <name><text><datetime>, RemoveEvent <name><text>,");
+        System.out.println("AddRandomTimeEvent <name><text><dateFrom><dateTo>, CloneEvent <name><text><nameTo>, ShowInfo <name>, StartScheduling");
+        System.out.println("---------------------------------------------------------------------");
+
+
+
+    }
+
+}
