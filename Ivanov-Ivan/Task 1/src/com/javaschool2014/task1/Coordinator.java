a8a87498c9d0d0141bc86d2071519685b78c5d0d
TARGETrus <mousebreaker@mail.ru>
2014-07-02 09:46:42 +0400

    Task 1 1.1 commit

--- Ivanov-Ivan/Task 1/src/com/javaschool2014/task1/Coordinator.java
+++ Ivanov-Ivan/Task 1/src/com/javaschool2014/task1/Coordinator.java
@@ -16,62 +16,11 @@ import java.util.regex.Pattern;
 
 public class Coordinator extends TimerTask implements Constants {
 
-    private String appName             = "Planfx v0.1";
-    private JFrame newFrame            = new JFrame(appName);
-
     DateFormat dateFormat              = new SimpleDateFormat(DATE_FORMAT);
     Timer timer                        = new Timer(true);
     public TreeMap<String, User> users = new TreeMap<String, User>();
 
     public  Coordinator () {
-
-    }
-
-    public void display() {
-
-        JPanel mainPanel = new JPanel();
-        mainPanel.setLayout(new BorderLayout());
-
-        JPanel leftPanel = new JPanel();
-        leftPanel.setLayout(new BorderLayout());
-
-        JPanel rightPanel = new JPanel();
-        rightPanel.setLayout(new BorderLayout());
-
-        JTextArea textArea = new JTextArea();
-        textArea.setEditable(false);
-        textArea.setLineWrap(true);
-        textArea.setFont(new Font("Serif", Font.PLAIN, 14));
-
-        JButton Create             = new JButton("Create");
-        JButton Modify             = new JButton("Modify");
-        JButton AddEvent           = new JButton("AddEvent");
-        JButton RemoveEvent        = new JButton("RemoveEvent");
-        JButton AddRandomTimeEvent = new JButton("AddRandomTimeEvent");
-        JButton CloneEvent         = new JButton("CloneEvent");
-        JButton ShowInfo           = new JButton("ShowInfo");
-        JButton StartScheduling    = new JButton("StartScheduling");
-        JButton StopScheduling     = new JButton("StopScheduling");
-
-        leftPanel.add(new JScrollPane(textArea), BorderLayout.CENTER);
-
-        rightPanel.add(Create);
-        rightPanel.add(Modify);
-        rightPanel.add(AddEvent);
-        rightPanel.add(RemoveEvent);
-        rightPanel.add(AddRandomTimeEvent);
-        rightPanel.add(CloneEvent);
-        rightPanel.add(ShowInfo);
-        rightPanel.add(StartScheduling);
-        rightPanel.add(StopScheduling);
-
-        mainPanel.add(BorderLayout.WEST, leftPanel);
-        mainPanel.add(BorderLayout.EAST, rightPanel);
-
-        newFrame.add(mainPanel);
-        newFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        newFrame.setSize(470, 300);
-        newFrame.setVisible(true);
 
     }
 
