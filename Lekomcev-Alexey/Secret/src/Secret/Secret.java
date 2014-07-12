2e76fa18fb2815bedb37aff267b042c2aedc404f
alekomcev <alexey.lekomcev@yandex.ru>
2014-07-12 09:13:42 +0400

    Task7

---
+++ Lekomcev-Alexey/Secret/src/Secret/Secret.java
@@ -0,0 +1,12 @@
+package Secret;
+
+ class Secret {
+    private static java.lang.String secret;
+
+    public Secret() { secret = "reflection"; }
+
+    private static java.lang.String crypt(java.lang.String crypted) {
+        System.out.println(crypted);
+        return "";
+    }
+}
