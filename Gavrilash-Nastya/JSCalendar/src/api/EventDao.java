8bdabd91c0ab43806a08f3a8d26f563dcaa39b66
ksun <hlor99@gmail.com>
2014-07-14 19:25:56 +0400

    found some errors of merging, very sorry =(

--- Gavrilash-Nastya/JSCalendar/src/api/EventDao.java
+++
@@ -1,9 +0,0 @@
-package api;
-
-import centralStructure.Event;
-
-public interface EventDao {
-	public void putEventToBase(Event e);
-
-	public void removeEventFromBase(Event e);
-}
--- Gavrilash-Nastya/JSCalendar/src/api/UserDao.java
+++
@@ -1,9 +0,0 @@
-package api;
-
-import centralStructure.User;
-
-public interface UserDao {
-	public void putUserToBase(User u);
-
-	public void changeUser(User u);
-}
